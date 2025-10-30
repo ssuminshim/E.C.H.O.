2c46d97edc6bad9faaf192a1bc988d0ca55b8dba
Chasy <chaseyeon02@gmail.com>
2025-10-30 23:59:50 +0900

    Intro(인트로)씬 구현
    [ 추가로 할 것 ]
    - 인트로 화면 전환 효과

--- ECHO/Assets/Scripts/IntroManager.cs
+++ ECHO/Assets/Scripts/IntroManager.cs
@@ -1,12 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class Intro : MonoBehaviour
 {
+    [SerializeField]
+    private Image image1;
+    [SerializeField]
+    private Image image2;
+    [SerializeField]
+    private Image image3;
+    [SerializeField]
+    private Image image4;
+
+
     public Text ScriptText_dialogue;
     public Text ScriptText_instruction;
     public string[] dialogue; // 인스펙터 창에서 대화 내용 수정 가능
@@ -19,6 +26,9 @@ public class Intro : MonoBehaviour
         {
             ScriptText_dialogue.text = dialogue[0];
         }
+
+        // 게임 시작 시 dialogue_count=0에 맞춰 이미지 초기화
+        UpdateImageActivation();
     }
 
     void Update()
@@ -31,6 +41,9 @@ public class Intro : MonoBehaviour
             {
                 dialogue_count++;
                 ScriptText_dialogue.text = dialogue[dialogue_count];
+
+                // dialogue_count가 증가할 때마다 이미지 활성화 상태 업데이트
+                UpdateImageActivation();
             }
             // 마지막 대화가 끝나면 씬 전환
             else
@@ -38,6 +51,40 @@ public class Intro : MonoBehaviour
                 SceneManager.LoadScene("Core");
                 dialogue_count = 0; // 필요 시 초기화
             }
+        }
+    }
+
+    /// <summary>
+    /// dialogue_count 값에 따라 이미지들의 활성화 상태를 업데이트
+    /// </summary>
+    private void UpdateImageActivation()
+    {
+        // 모든 이미지 비활성화
+        image1.gameObject.SetActive(false);
+        image2.gameObject.SetActive(false);
+        image3.gameObject.SetActive(false);
+        image4.gameObject.SetActive(false);
+
+        // dialogue_count 값에 따라 특정 이미지 활성화
+        switch (dialogue_count)
+        {
+            case 0:
+                image1.gameObject.SetActive(true);
+                break;
+            case 1:
+                image1.gameObject.SetActive(true);
+                image2.gameObject.SetActive(true);
+                break;
+            case 2:
+                image3.gameObject.SetActive(true);
+                break;
+            case 3:
+            case 4:
+                image3.gameObject.SetActive(true);
+                image4.gameObject.SetActive(true);
+                break;
+            default:
+                break;
         }
     }
 
