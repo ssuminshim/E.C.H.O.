54ac4952584cb990b6ba5878be74260824acfd49
Chasy <chaseyeon02@gmail.com>
2025-11-10 08:01:55 +0900

    화면 해상도 문제 해결

--- ECHO/Assets/Scripts/#00Server/#01Loading/LoadingScenario.cs
+++ ECHO/Assets/Scripts/#00Server/#01Loading/LoadingScenario.cs
@@ -16,11 +16,6 @@ public class LoadingScenario : Login
 		// 활성화되지 않은 상태에서도 게임이 계속 진행
 		Application.runInBackground = true;
 
-		// 해상도 설정 (9:18.5, 1440x2960)
-		int width	= Screen.width;
-		int height	= (int)(Screen.width * 18.5f / 9);
-		Screen.SetResolution(width, height, true);
-
 		// 화면이 꺼지지 않도록 설정
 		Screen.sleepTimeout = SleepTimeout.NeverSleep;
 
